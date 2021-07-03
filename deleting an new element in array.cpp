2564197021d6ea1b9a0738943401b3ae3bdbf10f
Gaurav Tiwari <tiwarigaurav8642@gmail.com>
2021-07-03 02:15:14 +0200

    value init

--- deleting an new element in array.cpp
+++ deleting an new element in array.cpp
@@ -2,7 +2,7 @@
 #include<conio.h>
 int main()
 {
-	int i,n,pos,value,a[10],del;
+	int i,n,pos,value,a[10],del;//initialization
 	printf("enter the number of element in array");
 	scanf("%d",&n);
 	printf("Enter the elements");

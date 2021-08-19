4efe75e7715c8c56ed17893a8e4756289cb664f6
Gaurav Tiwari <tiwarigaurav8642@gmail.com>
2021-08-19 23:33:33 +0200

    Update factor cal.cpp
    loop

--- factor cal.cpp
+++ factor cal.cpp
@@ -5,7 +5,7 @@ int main()//main funf
 	int i,n;// initialization of integers
 	printf("Enter any number:");// inter any number
 	scanf("%d",&n);// scan that number
-	for(i=1;i<=n;i++)
+	for(i=1;i<=n;i++)// for loop
 	{
 		if(n%i==0)
 		printf("the factor is :%d\n",i);

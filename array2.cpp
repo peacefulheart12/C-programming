0d0b28e0e05f689873cd17de9c2ea7a1dd52b37d
Gaurav Tiwari <tiwarigaurav8642@gmail.com>
2021-08-24 23:03:28 +0200

    Update array2.cpp
    print

--- array2.cpp
+++ array2.cpp
@@ -4,7 +4,7 @@ int main()// main function
 {
 	int x [10],i,sum;//array formation
 	
-	printf("enter any number");
+	printf("enter any number");//print
 	scanf("%d",&x[10]);
 	for(i=0;i<10;i++)
 	sum=i+i;
